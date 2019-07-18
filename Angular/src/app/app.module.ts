743435ee40435c0dd49ced0f5e3e6a32b35e06da
Theo <haoyuan.guo@clearbridgemobile.com>
2019-07-18 15:12:38 -0400

    Basic routing
    + added routing between components

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ManagementComponent } from './management/management.component';
+import { ServiceComponent } from './service/service.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    ManagementComponent,
+    ServiceComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
